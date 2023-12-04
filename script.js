eaa95e6e909e3b7e6362b5a089c9a7381f490e71
shader-boy <84195555+shader-boy@users.noreply.github.com>
2023-12-04 21:57:18 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,74 @@
 "use strict";
 
-// const currentTask = [];
-// const completedTask = [];
-
-// const addTask = function(task) {
-//     currentTask.push(task);
-// };
-
-// const removeTask = function(task) {
-//     const index = currentTask.indexOf(task);
-//     currentTask.splice(index, 1)
-// };
-
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+const greeting = document.querySelector(".greeting");
+const addButton = document.querySelector(".add-button");
 
-function addTask() {
+const cityName = function () {
+  const coordinates = navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude);
+    },
+    function () {
+      alert("Couldn't get your position");
+    }
+  );
+
+  // const apiKey =
+  // fetch(`https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${apiKey}`)
+};
+
+const greetingMessage = function () {
+  const now = new Date();
+  const dayOfWeek = now.getDay();
+  const monthOfYear = now.getMonth();
+  const dayOfMonth = now.getDate();
+  const year = now.getFullYear();
+  let dayPart;
+
+  const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  if (now.getHours() < 5) {
+    dayPart = "night";
+  } else if (now.getHours() < 12) {
+    dayPart = "morning";
+  } else if (now.getHours() < 18) {
+    dayPart = "afternoon";
+  } else if (now.getHours() >= 18) {
+    dayPart = "evening";
+  } else {
+    dayPart = "day";
+  }
+
+  greeting.innerHTML = `Good ${dayPart}! Today is ${weekdays[dayOfWeek]}, ${months[monthOfYear]} ${dayOfMonth}, ${year}.`;
+};
+
+const addTask = function () {
   if (inputBox.value === "") {
     alert("You must write something");
   } else {
@@ -27,26 +80,38 @@ function addTask() {
     li.appendChild(span);
   }
   inputBox.value = "";
-  saveData ();
-}
-
-listContainer.addEventListener("click", function(e) {
-    if(e.target.tagName ==='LI') {
-        e.target.classList.toggle('checked');
-        saveData ();
-    }
-    else if (e.target.tagName === "SPAN") {
-        e.target.parentElement.remove();
-        saveData ();
-    }
-}, false)
-
-function saveData () {
-    localStorage.setItem("data", listContainer.innerHTML);
-}
-
-function showTask() {
-    listContainer.innerHTML = localStorage.getItem("data");
+  saveData();
 };
 
+inputBox.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    addTask();
+  }
+});
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+const saveData = function () {
+  localStorage.setItem("data", listContainer.innerHTML);
+};
+
+const showTask = function () {
+  listContainer.innerHTML = localStorage.getItem("data");
+};
+
+greetingMessage();
 showTask();
+
+console.log(cityName);
